9b86fd1fa635bc97db085f11c1aef90545ca3f28
Denys Kuts <kutsdenys@gmail.com>
2019-07-31 11:42:24 +0300

    Updates

---
+++ precache-manifest.cbf348de936fc07d33bec0908b698dd1.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1de682b2761403616861430ab8ed50b9",
+    "url": "/denysk-powercode/graphic-editor-test/index.html"
+  },
+  {
+    "revision": "78e10064f148caa96ea2",
+    "url": "/denysk-powercode/graphic-editor-test/static/js/2.186ddc0f.chunk.js"
+  },
+  {
+    "revision": "dbeef93efb16a82120e6",
+    "url": "/denysk-powercode/graphic-editor-test/static/js/main.1a958c7e.chunk.js"
+  },
+  {
+    "revision": "53628f0fddf4ea19a319",
+    "url": "/denysk-powercode/graphic-editor-test/static/js/runtime~main.b1df2634.js"
+  }
+]);
